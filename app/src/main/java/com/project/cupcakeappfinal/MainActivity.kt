e3e05e8acd0470a043173c96feb7079f3fe33a45
Javier <javdevia@gmail.com>
2023-10-25 16:04:46 -0300

    ViewModel started

--- app/src/main/java/com/project/cupcakeappfinal/MainActivity.kt
+++ app/src/main/java/com/project/cupcakeappfinal/MainActivity.kt
@@ -28,6 +28,7 @@ class MainActivity : AppCompatActivity(R.layout.activity_main){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // NavHost
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
