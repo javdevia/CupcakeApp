17fe9ce28a9c844d01e35b07138f008f85b688cb
Javier <javdevia@gmail.com>
2023-11-02 13:35:32 -0300

    ViewModel Tests implemented

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,10 @@ dependencies {
     implementation ("androidx.navigation:navigation-fragment-ktx:2.7.4")
     implementation ("androidx.navigation:navigation-ui-ktx:2.7.4")
 
+    // Tests
+
+    androidTestImplementation ("androidx.arch.core:core-testing:2.1.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
